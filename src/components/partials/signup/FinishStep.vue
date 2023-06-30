819ba0b80f929cdfecab8942d3780a5844126ec5
peter <peter.johansson.king@gmail.com>
2023-06-30 10:25:58 -0700

    fix: finish step

--- src/components/partials/signup/FinishStep.vue
+++ src/components/partials/signup/FinishStep.vue
@@ -3,27 +3,31 @@
   <q-step :name="9" title="Finish" prefix="9" :done="step > 9">
     <!-- BEGIN: Verification -->
     <div class="row justify-between">
-      <q-input
-        filled
-        v-model="verifycode"
-        label="Verification Code"
-        style="width: 80%"
-      />
-      <q-btn color="primary" label="Verify" />
+      <div class="col-9">
+        <q-input
+          filled
+          v-model="vuelidate.verifycode.$model"
+          :error="vuelidate.verifycode.$error"
+          @blur="vuelidate.verifycode.$touch"
+          label="Verification Code *"
+          hide-bottom-space
+        />
+      </div>
+      <div class="col-2" style="margin: auto">
+        <q-btn color="primary" label="Verify" :disabled="vuelidate.$invalid" />
+      </div>
     </div>
     <!-- END: Verification -->
     <q-separator color="grey" class="q-mt-md" />
 
     <!-- BEGIN: Next and Back Button Group -->
     <q-stepper-navigation>
-      <q-btn color="primary" label="Complete Registration" />
       <q-btn
-        flat
-        @click="setPage(8)"
         color="primary"
-        label="Back"
-        class="q-ml-sm"
+        label="Complete Registration"
+        @submit="onSubmit()"
       />
+      <q-btn @click="setPage(8)" color="grey" label="Back" class="q-ml-sm" />
     </q-stepper-navigation>
     <!-- END: Next and Back Button Group -->
   </q-step>
@@ -31,11 +35,40 @@
 </template>
 
 <script>
+import useVuelidate from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
+import { reactive } from "vue";
 export default {
   props: {
     step: Number,
     setPage: Function,
   },
-  setup() {},
+  setup() {
+    const rules = {
+      verifycode: { required },
+    };
+    const form = reactive({
+      verifycode: "",
+    });
+
+    const vuelidate = useVuelidate(rules, form);
+    async function validate() {
+      return vuelidate.value.$validate();
+    }
+    const onNext = async (number) => {
+      try {
+        const valid = await validate();
+        if (!valid) {
+          return console.log("Form could not be submitted.");
+        }
+        props.setPage(number);
+      } catch (error) {
+        console.error("Login error");
+      }
+    };
+    return {
+      vuelidate,
+    };
+  },
 };
 </script>
