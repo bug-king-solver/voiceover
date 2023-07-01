af26341b12262ad55949390bf894e2ece9386c2d
peter <peter.johansson.king@gmail.com>
2023-07-01 14:47:16 -0700

    fix: banner component

---
+++ src/components/common/CustomBanner.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-banner rounded class="bg-cyan-2">
+    <template v-slot:avatar>
+      <img :src="imgUrl" style="width: 32px; height: 32px" />
+    </template>
+    {{ desc }}
+  </q-banner>
+</template>
+<script>
+export default {
+  props: {
+    desc: String,
+    src: String,
+  },
+  setup(props) {
+    const imgUrl = "/assets/img/" + props.src + ".png";
+    console.log(imgUrl);
+    return {
+      imgUrl,
+    };
+  },
+};
+</script>
